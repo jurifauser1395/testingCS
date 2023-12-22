b0d4912ec6ef3e56557090bc24593c735369a3ae
jurifauser1395 <juri.fauser@pm.me>
2023-12-22 22:22:23 +0100

    testing out primitiv data types

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System;
+
 namespace ConsoleApplication1
 {
     internal class Program
@@ -16,6 +18,17 @@ namespace ConsoleApplication1
                System.Console.WriteLine("Math: " + 3 + 4);
                System.Console.WriteLine("Math: " + (3 + 4) + " only works within parentheses");*/
             
+            // Primitive data types
+            int i = 3455;
+            double i01 = 3453.443543;
+            char x = 'y';
+            string name = "j.f.";
+            bool y = true;
+            
+            // Printout vars
+            Console.WriteLine("All primitive data types in c#: integer: " + i + ", double: " + i01 + ", char: " + x + ", string: " + name + ", bool: " + y);
+            
+
         }
     }
 }
