d6c81d5d23d89415163af880d2dbfbc4657e7309
jurifauser1395 <jurifauser@pm.me>
2024-01-01 22:47:25 +0100

    add getters and setters properies to the car class

--- ConsoleApplication1/Car.cs
+++ ConsoleApplication1/Car.cs
@@ -7,8 +7,24 @@ namespace ConsoleApplication1
         public String Color;
         public String Form;
         public int HP;
-        
 
 
+        public string Color1
+        {
+            get => Color;
+            set => Color = value;
+        }
+
+        public string Form1
+        {
+            get => Form;
+            set => Form = value;
+        }
+
+        public int Hp
+        {
+            get => HP;
+            set => HP = value;
+        }
     }
 }
