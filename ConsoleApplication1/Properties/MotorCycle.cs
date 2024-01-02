bd6eef225bd58447220a54801bce68bf6fc385ee
jurifauser1395 <jurifauser@pm.me>
2024-01-02 11:45:57 +0100

    add motorcycle class

---
+++ ConsoleApplication1/Properties/MotorCycle.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApplication1.Properties
+{
+    public class MotorCycle : Vehicle
+
+    {
+        public override void Honk()
+        {
+            System.Console.WriteLine("\nbrrr, brrr!");
+        }
+    }
+}
